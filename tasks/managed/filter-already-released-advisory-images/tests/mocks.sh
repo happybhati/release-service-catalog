2647c5b0dec8e5abd34627c38e7691991743b126
Happy Bhati <hbhati@redhat.com>
2025-05-01 00:29:29 -0400

    add test
    Signed-off-by: Happy Bhati <hbhati@redhat.com>

--- tasks/managed/filter-already-released-advisory-images/tests/mocks.sh
+++ tasks/managed/filter-already-released-advisory-images/tests/mocks.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -ex
 
+# mocks to be injected into task step scripts
+
 function kubectl() {
-  if [[ "$*" == "get internalrequest "*"-o=jsonpath={.status.results}" ]]; then
+  # The IR won't actually be acted upon, so mock it to return Success as the task wants
+  if [[ "$*" == "get internalrequest "*"-o=jsonpath={.status.results}" ]]
+  then
     echo '{
       "result": "Success",
-      "filtered_snapshot": "{\"application\":\"myapp\",\"components\":[{\"name\":\"comp1\",\"repository\":\"quay.io/redhat-prod/repo\"}]}"
-    }' 
+      "filtered_snapshot": "{\"application\":\"myapp\",\"components\":[{\"name\":\"new-component\",\"repository\":\"quay.io/redhat-pending/repo2\",\"containerImage\":\"quay.io/redhat-pending/repo2@sha256:def456\"}]}",
+      "internalRequestPipelineRunName": "test-pipeline-run",
+      "internalRequestTaskRunName": "test-task-run"
+    }'
   else
-    /usr/bin/kubectl $*
+    /usr/bin/kubectl "$@"
   fi
 }
